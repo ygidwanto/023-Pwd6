b8417f5312f1e19338c3db596d4d95332e45632d
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-04-05 21:53:10 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Sebuah video kucing galak");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audionya")
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini adalah Gambar Kucing")
+});
